1e20497ed43bbe8cb7eede0caec0c080a6a5efe7
Mo-21 <m.malaeb21@gmail.com>
2023-09-05 01:37:24 +0300

    Add Task Progress Toggle

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,8 @@ const addButton = document.getElementById("add-button");
 const container = document.querySelector(".container");
 const inputs = document.querySelectorAll(".form-control");
 const radioButtons = document.querySelectorAll(".btn-check");
+const taskInput = document.getElementById("task-input");
+const dueDateInput = document.getElementById("due-date-input");
 
 let arrayOfTasks = [];
 getTasksFromLocalStorage();
@@ -17,24 +19,22 @@ if (localStorage.getItem("tasks")) {
 
 addButton.onclick = function () {
   let inputsValuesArray = [];
-  inputs.forEach((input) => {
-    inputsValuesArray.push(input.value);
-  });
 
-  // radioButtons.forEach((radioButton) => {
-  //   radioButton.addEventListener("click", (e) => {
-  //     // Check if the radio button is checked
-  //     if (radioButton.hasAttribute('checked')) {
-  //       // Get the selected value
-  //       inputsValuesArray.push(e.target);
-  //     }
-  //   });
-  // });
-
-  addTasksToArray(inputsValuesArray);
-  inputs.forEach((input) => {
-    input.value = "";
-  });
+  if (taskInput.value !== "" && dueDateInput.value !== "") {
+    inputs.forEach((input) => {
+      inputsValuesArray.push(input.value);
+    });
+    addTasksToArray(inputsValuesArray);
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+  } else {
+    const alertDiv = document.getElementById("alert");
+    alertDiv.style.display = "block";
+    setTimeout(() => {
+      alertDiv.style.display = "none";
+    }, 5000);
+  }
 };
 
 function addTasksToArray(inputsValuesArray) {
@@ -52,29 +52,61 @@ function addTasksToScreen(arrayOfTasks) {
   container.innerHTML = "";
 
   arrayOfTasks.forEach((task) => {
+    const divTask = document.createElement("div");
+    divTask.setAttribute("data-id", task.id);
+    divTask.className = "task";
+
     const divTaskTitle = document.createElement("div");
-    divTaskTitle.className = "task";
+    divTaskTitle.className = "task-title";
+    divTaskTitle.classList.add("editable-text");
 
     if (task.completed) {
       divTaskTitle.className = "task done";
     }
 
-    divTaskTitle.setAttribute("data-id", task.id);
-    divTaskTitle.appendChild(document.createTextNode(task.task));
-    const span = document.createElement("span");
-    span.className = "del";
-    span.appendChild(document.createTextNode("Delete"));
+    divTaskTitle.appendChild(document.createTextNode(`Task: ${task.task}`));
+
+    const spanDel = document.createElement("span");
+    spanDel.className = "del";
+    spanDel.appendChild(document.createTextNode("Delete"));
+
+    const spanDone = document.createElement("span");
+    spanDone.className = "done-task";
+    spanDone.appendChild(document.createTextNode("Done"));
+
+    const spanSave = document.createElement("span");
+    spanSave.innerText = "Save";
+    spanSave.className = "save-task";
+    spanSave.style.display = "none"; // Initially, the save button is hidden
+    spanSave.onclick = function () {
+      saveEditedText(divTaskTitle);
+    };
+
+    const spanEdit = document.createElement("span");
+    spanEdit.className = "edit-task";
+    spanEdit.appendChild(document.createTextNode("Edit"));
+    spanEdit.onclick = function (event) {
+      editTextTitle(divTaskTitle, event);
+    };
 
     const divTaskDueDate = document.createElement("div");
     const divTaskDescription = document.createElement("div");
-    divTaskDueDate.appendChild(document.createTextNode(task.dueDate));
+    divTaskDescription.classList.add("note");
+    divTaskDueDate.appendChild(
+      document.createTextNode(`Due Date: ${task.dueDate}`)
+    );
+    divTaskDueDate.classList.add("due-date");
     divTaskDescription.appendChild(document.createTextNode(task.description));
     // Append Other Task Elements To Title Div
-    divTaskTitle.appendChild(span);
-    divTaskTitle.appendChild(divTaskDueDate);
-    divTaskTitle.appendChild(divTaskDescription);
+    divTask.appendChild(spanDel);
+    divTask.appendChild(spanDone);
+    divTask.appendChild(spanEdit);
+    divTask.appendChild(spanSave);
+    divTask.appendChild(divTaskTitle);
+    divTask.appendChild(divTaskDueDate);
+    divTask.appendChild(divTaskDescription);
     // Add Task Div To Tasks Container
-    container.appendChild(divTaskTitle);
+    container.appendChild(divTask);
   });
 }
 
@@ -84,11 +116,68 @@ container.addEventListener("click", (e) => {
     deleteTasksFromStorage(e.target.parentElement.getAttribute("data-id"));
     e.target.parentElement.remove();
   }
+
+  if (e.target.classList.contains("done-task")) {
+    changeTaskStatus(e.target.parentElement.getAttribute("data-id"));
+    e.target.parentElement.classList.toggle("done");
+  }
 });
 
 function deleteTasksFromStorage(taskId) {
   arrayOfTasks = arrayOfTasks.filter((task) => task.id != taskId);
   console.log(arrayOfTasks);
+  addTasksToLocalStorage(arrayOfTasks);
+}
+
+//toggle task status
+function changeTaskStatus(taskId) {
+  for (let i = 0; i < arrayOfTasks.length; i++) {
+    if (arrayOfTasks[i].id == taskId) {
+      arrayOfTasks[i].completed == false
+        ? (arrayOfTasks[i].completed = true)
+        : (arrayOfTasks[i].completed = false);
+    }
+  }
+  addTasksToLocalStorage(arrayOfTasks);
+}
+
+//edit title
+// function editTextTitle(divTaskTitle, event) {
+//   divTaskTitle.contentEditable = true;
+//   divTaskTitle.focus();
+
+//   const saveButton = document.querySelectorAll(".save-task");
+//   saveButton.forEach((save) => {
+//     if (
+//       event.target.parentElement.getAttribute("data-id") ===
+//       divTaskTitle.parentElement.getAttribute("data-id")
+//     ) {
+//       save.style.display = "inline";
+//     }
+//   });
+// }
+
+//save changes
+// function saveEditedText(divTaskTitle) {
+//   const editedText = divTaskTitle.innerText;
+//   const saveButton = document.querySelectorAll(".save-task");
+//   saveButton.forEach((save) =>
+//     addEventListener("click", () => {
+//       save.style.display = "none";
+//       createNewText(save.parentElement.getAttribute("data-id"), editedText);
+//     })
+//   );
+
+//   divTaskTitle.contentEditable = false;
+// }
+
+//apply changes
+function createNewText(taskId, editedText) {
+  for (let i = 0; i < arrayOfTasks.length; i++) {
+    if (arrayOfTasks[i].id == taskId) {
+      arrayOfTasks[i].task = editedText;
+    }
+  }
   addTasksToLocalStorage(arrayOfTasks);
 }
 
